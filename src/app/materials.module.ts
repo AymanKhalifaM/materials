0cc273141ca5f95f2b763b4fca0604f6f8f3d0b8
AymanKhalifaM <ayman.elsawy.kh94@gmail.com>
2020-09-17 00:31:08 +0200

    first commit

---
+++ src/app/materials.module.ts
@@ -0,0 +1,55 @@
+import { NgModule } from "@angular/core";
+import {
+  MatFormFieldModule,
+  MatInputModule,
+  MatIconModule,
+  MatButtonModule,
+  MatDatepickerModule,
+  MatNativeDateModule,
+  MatCheckboxModule,
+  MatSidenavModule,
+  MatToolbarModule,
+  MatListModule,
+  MatTabsModule,
+  MatCardModule,
+  MatSelectModule,
+  MatProgressSpinnerModule,
+  MatDialogModule
+} from "@angular/material";
+@NgModule({
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatCheckboxModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule,
+    MatTabsModule,
+    MatCardModule,
+    MatSelectModule,
+    MatProgressSpinnerModule,
+    MatDialogModule
+  ],
+  exports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatCheckboxModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule,
+    MatTabsModule,
+    MatCardModule,
+    MatSelectModule,
+    MatProgressSpinnerModule,
+    MatDialogModule
+  ],
+})
+export class MaterialsModule {}
---
+++ src/app/training/new-training/new-training.component.ts
@@ -0,0 +1,17 @@
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+
+@Component({
+  selector: 'app-new-training',
+  templateUrl: './new-training.component.html',
+  styleUrls: ['./new-training.component.css']
+})
+export class NewTrainingComponent implements OnInit {
+  @Output() trainingStrat = new EventEmitter<void>();
+  constructor() { }
+
+  ngOnInit() {
+  }
+  onStartTraining(){
+    this.trainingStrat.emit()
+  }
+}
